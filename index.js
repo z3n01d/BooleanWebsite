cba9248e68bbb98a46f1e1d53d5e13fed4d0896e
RealJace <72617776+Simonek-loki@users.noreply.github.com>
2022-05-26 19:32:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const info = document.getElementById("info");
 const invite = document.getElementById("invite");
 
 info.addEventListener("click",() => {
-    window.location.href = "info.html";
+    location.href = "info.html";
 });
 invite.addEventListener("click",() => {
-    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=977276791423119371&permissions=8&scope=bot%20applications.commands";
+    location.href = "https://discord.com/api/oauth2/authorize?client_id=977276791423119371&permissions=8&scope=bot%20applications.commands";
 });
