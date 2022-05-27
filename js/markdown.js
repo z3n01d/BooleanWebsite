1c9870d6ba711b684e628c363e20a2da918d0443
RealJace <72617776+Simonek-loki@users.noreply.github.com>
2022-05-27 19:27:06 +0200

    Update markdown.js

--- js/markdown.js
+++ js/markdown.js
@@ -1,19 +1,15 @@
-var fs = require("fs");
-var showdown = require("showdown");
-var converter = new showdown.Converter();
+require(["fs","showdown"],function() {
+    var converter = new showdown.Converter();
 
-document.onreadystatechange = () => {
-    if (document.readyState === "complete") {
-        var md = fs.readFileSync("../markdown/info.md");
-        var html = converter.makeHtml(md)
+    var md = fs.readFileSync("../markdown/info.md");
+    var html = converter.makeHtml(md)
 
-        var body = document.getElementById("markdown")
+    var body = document.getElementById("markdown")
         
-        console.log("Loading markdown...");
-        console.log(html);
+    console.log("Loading markdown...");
+    console.log(html);
 
-        body.innerHTML = html;
+    body.innerHTML = html;
 
-        console.log("Markdown ready!")
-    };
-};
+    console.log("Markdown ready!")
+});
